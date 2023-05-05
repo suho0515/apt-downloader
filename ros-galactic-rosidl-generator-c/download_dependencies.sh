5ba04f3845f2d52ee03fd0a03c2b600eb9dbeb7d
suho0515 <suho0515@gmail.com>
2023-05-05 15:05:48 +0900

    Initial commit

---
+++ ros-galactic-rosidl-generator-c/download_dependencies.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PACKAGE_NAME="ros-galactic-rosidl-generator-c"
+DEPENDENCIES=$(apt-cache depends --recurse --no-recommends --no-suggests --no-conflicts --no-breaks --no-replaces --no-enhances --no-pre-depends $PACKAGE_NAME | grep "^\w")
+
+for DEPENDENCY in $DEPENDENCIES; do
+  echo "Downloading $DEPENDENCY"
+  apt-get download $DEPENDENCY 2>/dev/null
+done
