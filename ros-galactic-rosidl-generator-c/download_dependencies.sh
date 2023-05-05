ffabacd57c4be0c7c456113e581028e4a9f3b82e
suho0515 <suho0515@gmail.com>
2023-05-05 18:44:58 +0900

    feat: edit to download just ros-galactic packages

--- ros-galactic-rosidl-generator-c/download_dependencies.sh
+++ ros-galactic-rosidl-generator-c/download_dependencies.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 PACKAGE_NAME="ros-galactic-rosidl-generator-c"
-DEPENDENCIES=$(apt-cache depends --recurse --no-recommends --no-suggests --no-conflicts --no-breaks --no-replaces --no-enhances --no-pre-depends $PACKAGE_NAME | grep "^\w")
+DEPENDENCIES=$(apt-cache depends --recurse --no-recommends --no-suggests --no-conflicts --no-breaks --no-replaces --no-enhances --no-pre-depends $PACKAGE_NAME | grep "^\w*ros-galactic")
+
+cd deb
 
 for DEPENDENCY in $DEPENDENCIES; do
   echo "Downloading $DEPENDENCY"
